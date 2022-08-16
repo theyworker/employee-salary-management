237bb7e722fb6b83d49cf5314c0ec617d63206f3
devaka_kum <k.devaka@directfn.com>
2022-08-17 00:43:18 +0530

    delete page

--- src/Dashboard/Sucess/Delete.js
+++ src/Dashboard/Sucess/Delete.js
@@ -1,15 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { baseUrl } from "../../const/static";
+import SpinFC from "antd/lib/spin";
 
 const Delete = () => {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.delete(`${baseUrl}/${id}`).then(res=> console.log(res.status));
+    axios.delete(`${baseUrl}/${id}`).then((res) => {
+      if (res.status == 200) {
+        setLoading(false);
+      }
+    });
   }, []);
-  return <div>Deleting {id}</div>;
+  return (
+    <div>
+      Deleting {id}
+      {loading ? <SpinFC size="large" /> : <React.Fragment />}
+    </div>
+  );
 };
 
 export default Delete;
--- src/Dashboard/Table/EmployeeTable.js
+++ src/Dashboard/Table/EmployeeTable.js
@@ -2,43 +2,52 @@ import React from "react";
 import { Table, Space } from "antd";
 import ConfirmPopUp from "./ConfirmPopUp";
 
-const columns = [
-  {
-    title: "ID",
-    dataIndex: "id",
-    key: "id",
-    sorter: (a, b) => a.id - b.id
-  },
-  {
-    title: "Name",
-    dataIndex: "fullName",
-    key: "fullName",
-  },
-  {
-    title: "User Name",
-    dataIndex: "userName",
-    key: "userName",
-    sorter: (a, b) => a.userName - b.userName,
-  },
-  {
-    title: "Salary",
-    dataIndex: "salary",
-    key: "salary",
-    sorter: (a, b) => a.salary - b.salary,
-  },
-  {
-    title: "Action",
-    key: "action",
-    render: (_, record) => (
-      <Space size="middle">
-        <a onClick={() => console.log(record.userName)}>Edit</a>
-        <ConfirmPopUp text='Delete' title='Are you sure?' confirm={()=>console.log('Deleting' ,record.userName)}/>
-      </Space>
-    ),
-  },
-];
+import { useNavigate } from "react-router-dom";
 
 const EmployeeTable = ({ data }) => {
+  let navigate = useNavigate();
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+      sorter: (a, b) => a.id - b.id,
+    },
+    {
+      title: "Name",
+      dataIndex: "fullName",
+      key: "fullName",
+    },
+    {
+      title: "User Name",
+      dataIndex: "userName",
+      key: "userName",
+      sorter: (a, b) => a.userName - b.userName,
+    },
+    {
+      title: "Salary",
+      dataIndex: "salary",
+      key: "salary",
+      sorter: (a, b) => a.salary - b.salary,
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <a onClick={() => console.log(record.userName)}>Edit</a>
+          <ConfirmPopUp
+            text="Delete"
+            title="Are you sure?"
+            confirm={() =>
+              navigate(`../delete/${record.id}`, { replace: true })
+            }
+          />
+        </Space>
+      ),
+    },
+  ];
+
   return (
     <div>
       <Table
@@ -46,7 +55,7 @@ const EmployeeTable = ({ data }) => {
         columns={columns}
         pagination={{ pageSize: 5 }}
         responsive
-        loading={data.length == 0 }
+        loading={data.length == 0}
       />
     </div>
   );
